5d07dba3ad22deb6088f58bcf0169b975eade870
Yahir <Luisyajairo@outlook.com>
2025-01-16 12:22:12 -0600

    correcion dbMysql

--- models/dbMysql.js
+++ models/dbMysql.js
@@ -6,7 +6,7 @@ const dbMysql = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "examendb1"
+    database: "examenbd"
 
 });
 dbMysql.connect((err) => {
